89e082b56530e865b48e7efd6943430e554ca953
najabenali <najjoubenali@gmail.com>
2024-12-06 16:45:15 +0100

    df

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/App.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './App';  // Import your App component
+import './styles/App.css';  // Import styles
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
